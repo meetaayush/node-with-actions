e088de93732fc3eed049ef45797f12f430b69bf5
Ayush Srivastav <ayush@autonom8.com>
2020-09-06 19:26:48 +0530

    Added port and logging port_app

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ msg: "SOME RANDOM TEXT THIS TIME" });
+  res.send({ msg: "SOME RANDOM TEXT THIS TIME", port: process.env.PORT_APP });
 });
 
-const PORT = process.env.PORT_APP;
+const PORT = process.env.PORT_APP || 80;
 
 app.listen(PORT, () => console.log(`SERVER IS RUNNING AT PORT ${PORT}`));
 
