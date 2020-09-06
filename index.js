1aae19c4bd4570152ee0f65dae8b1a88d9edd828
Ayush Srivastav <ayush@autonom8.com>
2020-09-06 20:05:19 +0530

    Changed the text

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.json({ msg: "A different text this time", port: process.env.PORT_APP });
+  res.json({
+    msg: "A different new text this time",
+    port: process.env.PORT_APP,
+  });
 });
 
-const PORT = process.env.PORT_APP;
+const PORT = process.env.PORT_APP || 80;
 
 app.listen(PORT, () => console.log(`SERVER IS RUNNING AT PORT ${PORT}`));
 
